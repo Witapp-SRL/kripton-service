6e2b9485f6998384c1de5248b1f6b66da337add0
mirko-witapp <mirko.giulianini@witapp.it>
2025-11-13 10:50:40 +0100

    Create urls.py

---
+++ backend/project/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # API Endpoints per la nostra app 'portal_app'
+    path('api/', include('portal_app.urls')),
+    
+    # API Endpoints per l'autenticazione JWT
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
